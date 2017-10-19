e2c430c8f64aa3b1b5679b08c441bf35658e336c
Francis Pilon <francis-pilon@outlook.com>
2017-10-19 17:08:56 -0400

    Initial Commit
    Redacted Token for protection.
    
    Initial Commit of current work.

---
+++ turtle.js
@@ -0,0 +1,29 @@
+var graph = require('fbgraph');
+const fs = require('fs');
+const accessToken = *****
+
+function sendTurtle(){
+	graph.setAccessToken(accessToken);
+    var files = fs.readdirSync('./Turtle/');
+    var index = Math.floor(Math.random() * files.length)
+    var url = "http://www.offbyoneerror.ca/FileHosting/Turtle/" + files[index];
+    console.log(url);
+
+    var wallPost = {
+	message: '\uD83D\uDC22',
+        url: url
+    };
+
+    graph.post(******* + "/photos", wallPost, function(err, res) {
+        console.log(res); // { id: xxxxx}
+        fs.unlinkSync("./Turtle/" + files[index]);
+    });
+
+    setTimeout(function() {}, 3600000);
+}
+
+sendTurtle();
+
+
+
+
